f52283122fffe5bdf698542e4b5f2dca2ed920a9
Marius Cristea <marius.cristea@irian.ro>
2015-05-06 11:28:11 +0300

    L08 - Server send - final

--- L08/components/product/review/review.directive.js
+++ L08/components/product/review/review.directive.js
@@ -5,12 +5,24 @@
         return {
             restrict: 'E',
             templateUrl: 'components/product/review/review.htm',
-            controller: function() {
+            controller: function(SERVER, $http) {
                 this.review = {};
+
                 this.addReview = function(product) {
                     this.review.createdOn = Date.now();
-                    product.reviews.push(this.review);
+                    this.saveReview(product, this.review);
                     this.review = {};
+                };
+
+                this.saveReview = function(product, review) {
+                    $http.put(SERVER.URL + ':' + SERVER.PORT + SERVER.PATH + 'pizzas/addReview/' + product._id, review)
+                        .success(function() {
+                            product.reviews.push(review);
+                            console.info('Added review: ' + review);
+                        })
+                        .error(function() {
+                            console.error('Error while saving: ' + product);
+                        });
                 }
             },
             controllerAs: 'reviewCtrl'
